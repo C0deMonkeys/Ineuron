6a1e5330dc93e24a8de93e0480e08c8b2f7b6f95
SIYACHIN <115447338+C0deMonkeys@users.noreply.github.com>
2022-10-17 14:13:24 +0530

    Add files via upload

---
+++ assigment4.js
@@ -0,0 +1,11 @@
+let n =0;
+function matchStickHouses(n)
+{
+    let step = n-1;     //common matchsticks in the step
+    let total = (n*6) - step;  //matchstick per house
+    console.log(total);   
+}
+
+matchStickHouses(87);
+
+
