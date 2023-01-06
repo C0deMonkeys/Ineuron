fbbe581c4e1c0e06ddd3089e79f544cc08bb3f78
SIYACHIN <115447338+C0deMonkeys@users.noreply.github.com>
2023-01-06 07:59:37 +0530

    Add files via upload

--- Assigment 9/Q1.js
+++ Assigment 9/Q1.js
@@ -1,1 +1,19 @@
+// 1. Carefully observe this example.
+// a) Is the InnerFunction() a closure?
+// b) What is output of this program
 
+
+function OuterFunction()
+{ var outerVariable = 100;
+function InnerFunction() {
+alert(outerVariable);
+}
+return InnerFunction;
+}
+var innerFunc = OuterFunction();
+innerFunc();
+
+
+// a) Yes. InnerFunction is a closure
+
+// b) Code would run fine in browser, using node js alert is giving error
